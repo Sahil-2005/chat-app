c73797cde2385d0ea4f92ffb71da3324c7f4a212
Sahil Gawade <104153594+Sahil-2005@users.noreply.github.com>
2025-04-12 19:35:08 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authRoutes from "./routes/auth-route.js";
 import dotenv from 'dotenv';
+
+import path from 'path'
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js';
@@ -12,7 +14,8 @@ dotenv.config()
 // const app = express();
 
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // app.use(express.json());
 app.use(express.json( {limit: '10mb'} ));
@@ -28,6 +31,18 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
+
+
 
 
 server.listen(PORT, () => {
